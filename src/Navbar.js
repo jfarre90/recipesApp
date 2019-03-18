dd72b773924d3b2996b71fe08d95068046d278f2
Javier Farre <jfarre90@gmail.com>
2019-03-18 09:47:59 +0000

    added state

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,9 +1,6 @@
 import React, {Component} from 'react';
 import './Navbar.css';
 
-import React, {Component} from 'react';
-import './Navbar.css';
-
 class Navbar extends Component {
   render() {
     return (
--- src/Recipe.js
+++ src/Recipe.js
@@ -8,16 +8,19 @@ class Recipe extends Component {
     ingredients: PropTypes.arrayOf(PropTypes.string).isRequired,
     instructions: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    onDelete: PropTypes.func.isRequired
   }
+  
   render() {
-    const {title, img, instructions} = this.props; // This is the same as title = this.props.title
-    const ingredients = this.props.ingredients.map((ing,index)=> (
-      <li key={index}>{ing}</li>  
-    )); //because we are using an arrow function where we are encapsulating everything in parentesis, we dont need to specify return.
-    return(
-      <div>
-        <div className="recipe-card">
-          <img className="recipe-card-img" src={img} alt={title} />
+    const {title, img, instructions, id, onDelete} = this.props;
+    const ingredients = this.props.ingredients.map((ing, index) => (
+      <li key={index}>{ing}</li> 
+    ));
+    return (
+      <div className="recipe-card">
+        <div className="recipe-card-img">
+          <img src={img} alt={title} />
         </div>
         <div className="recipe-card-content">
           <h3 className="recipe-title">{title}</h3>
@@ -25,12 +28,12 @@ class Recipe extends Component {
           <ul>
             {ingredients}
           </ul>
-          <h4>Instructions: </h4>
+          <h4>Instructions:</h4>
           <p>{instructions}</p>
+          <button type="button" onClick={() => onDelete(id)}>DELETE</button>
         </div>
-      </div> //this div is added because we can never return multiple jsx elements next to each other.
+      </div>
     );
-    
   }
 }
 

3ca720b8e26f198bed90cf67cb7bb49d4ae41408
Javier Farre <jfarre90@gmail.com>
2019-03-19 12:10:49 +0000

    Updates

---
+++ precache-manifest.d47cb01b4949e6d290b481a8954ee12c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74f2901ded20a6a6fad5",
+    "url": "/recipesApp/static/css/main.8a4b87e6.chunk.css"
+  },
+  {
+    "revision": "74f2901ded20a6a6fad5",
+    "url": "/recipesApp/static/js/main.3d9a4357.chunk.js"
+  },
+  {
+    "revision": "92699adbeacb0cdef8d0",
+    "url": "/recipesApp/static/js/runtime~main.7869bdde.js"
+  },
+  {
+    "revision": "18cc0ec54804ce0e57c7",
+    "url": "/recipesApp/static/js/2.ad220ec5.chunk.js"
+  },
+  {
+    "revision": "e766a692780251dca48b67a2b589f1fd",
+    "url": "/recipesApp/index.html"
+  }
+];
